e7d2bbec808fad2885870fbf4184da7754f5bf55
Ravi Terala <rterala@gmail.com@users.noreply.github.com>
2021-06-12 10:51:37 -0700

    vimrc fold method fix and setup.sh cleanup

--- setup.sh
+++ setup.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
-function install_brew() {
-    echo "Installing xcode tools"
-    xcode-select --install
+function install_xcode_commandline_tools() {
+    echo -n "Installing XCode commandline tools : "
 
-    echo "Installing brew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    xcode-select -p 1>/dev/null
+    xcode_installed=$?
+    if [ $xcode_installed -eq 2 ];
+    then
+        echo "Installing xcode tools"
+        xcode-select --install
+        echo "[ Installed ]"
+    else
+        echo "[ Skipping ]"
+    fi
 }
 
+function install_brew() {
+    echo -n "Installing brew :                  "
+    if ! command -v brew &> /dev/null
+    then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "[ Installed ]"
+    else
+        echo "[ Skipping ]"
+    fi
+}
 
-if ! command -v brew &> /dev/null
-then
-    install_brew
-else
-    echo "brew is already installed. Skipping."
-fi
+function install_ansible() {
+    echo -n "Installing ansible :               "
+
+    if ! command -v ansible &> /dev/null
+    then 
+        brew install ansible
+        echo "[ Installed ]"
+    else
+        echo "[ Skipping ]"
+    fi
+}
+
+install_xcode_commandline_tools
+install_brew
+install_ansible
 
 echo "Installing galaxy roles"
 ansible-galaxy role install -r requirements.yml
@@ -22,12 +48,6 @@ ansible-galaxy role install -r requirements.yml
 echo "Installing galaxy collections"
 ansible-galaxy collection install -r requirements.yml
 
-
-if ! command -v ansible &> /dev/null
-then 
-    echo "Installing ansible via brew"
-    brew install ansible
-fi
-
 echo "Installing tools"
 ansible-playbook --ask-become-pass setup.yml
+
